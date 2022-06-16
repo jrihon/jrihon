e8effa7c0f3537924bfe18dad038dfe255e61bc9
jrihon <73099644+jrihon@users.noreply.github.com>
2022-06-16 14:17:14 +0200

    Update gotodirectory.sh

--- gotodirectory.sh
+++ gotodirectory.sh
@@ -6,7 +6,7 @@ local dir
 # Assumes directory .homebrew_scripts/ and .homebrew_scripts/search_directory/ exist. These are just personal choices not to overrun
 #  my $HOME directory
 # Open a terminal and pipe the `find all non-hidden directories in $HOME and pipe that into a file`
-/usr/bin/gnome-terminal --wait -- bash -c \
+/usr/bin/gnome-terminal --geometry 160x40 --wait -- bash -c \
    'dir=$(find . -not -path $HOME -type d -print 2> /dev/null | fzf +m > $HOME/.homebrew_scripts/search_directory/dir.txt)'
 
 # Read the contents of the file, change the `.` symbol by $HOME and open the directory in a terminal
