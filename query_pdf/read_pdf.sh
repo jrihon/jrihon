c86acb52770c996e14d97c3bb93cb9be96ba1cf2
jrihon <73099644+jrihon@users.noreply.github.com>
2022-06-18 13:04:51 +0200

    Delete query_pdf directory

--- query_pdf/read_pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Cat the contents of the tmp_pdfname, which will be the name of the pdf you have queried
-# xargs it into evince, the default ubuntu pdf reader
-
-cat $HOME/query_pdf/tmp_pdfname | xargs -r evince &
