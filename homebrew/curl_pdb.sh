573d808c1123a8734a7c7593dfac60d5d1a03821
jrihon <73099644+jrihon@users.noreply.github.com>
2022-06-18 14:20:10 +0200

    Update curl_pdb.sh

--- homebrew/curl_pdb.sh
+++ homebrew/curl_pdb.sh
@@ -1,4 +1,8 @@
 #!/bin/env bash
 
-# Enter the pdb code of the file you want to download and save it instanteniously
+# save this file somewhere on your system, chmod +x, and alias this to access anywhere on your system
+
+
+# Enter the pdb code of the file you want to download through CLI and save it instanteniously
 curl https://files.rcsb.org/view/$1.pdb > ./$1.pdb
+
