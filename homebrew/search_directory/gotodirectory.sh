630afc7e9cb52827510ef6518d157949f86398b7
jrihon <73099644+jrihon@users.noreply.github.com>
2022-06-27 11:45:09 +0200

    bug fix to exclude any hidden directories in home

--- homebrew/search_directory/gotodirectory.sh
+++ homebrew/search_directory/gotodirectory.sh
@@ -7,7 +7,7 @@ local dir
 #  my $HOME directory
 # Open a terminal and pipe the `find all non-hidden directories in $HOME and pipe that into a file`
 /usr/bin/gnome-terminal --geometry 160x40 --wait -- bash -c \
-   'dir=$(find . -not -path $HOME -type d -print 2> /dev/null | fzf +m > $HOME/homebrew/search_directory/dir.txt)'
+   'dir=$(find $HOME -not -path "$HOME/.*" -type d -print 2> /dev/null | fzf +m > $HOME/homebrew/search_directory/dir.txt)'
 
 # Read the contents of the file, change the `.` symbol by $HOME and open the directory in a terminal
 dir=`cat $HOME/homebrew/search_directory/dir.txt | sed -e "s|\.|$HOME|g"` &&  /usr/bin/gnome-terminal --working-directory=$dir
